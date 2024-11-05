3350fc6cdda3a5e33d7a34fdcebcf8d67cc67e78
Natyelly Neres <natyelly.neres77@gmail.com>
2024-11-05 20:51:09 -0300

    site

---
+++ site/site.js
@@ -0,0 +1,12 @@
+function mudar() {//chamar a função
+    if (document.body.style.backgroundColor === 'white') {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    }else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+
+    }
+
+
+}
