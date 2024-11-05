20096591b3844a5012202da5008b313058dd4a30
Natyelly Neres <natyelly.neres77@gmail.com>
2024-11-05 20:49:09 -0300

    exercicios

---
+++ JAVA/cartão/js.js
@@ -0,0 +1,13 @@
+function ira() {
+    let text = document.getElementById('info');
+    text.innerHTML = "fgasdgsdfgsdfgfsdg";
+    if (text.style.display === "none" ) {
+        text.style.display = "block"; // Mostra o texto
+    } else {
+        text.style.display = "none"; // Oculta o texto
+    }
+}
+    
+
+ 
+    
